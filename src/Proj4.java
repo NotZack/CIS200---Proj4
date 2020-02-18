588a601b9710a63ab2cffe45cf2bf3af87525c8d
NotZack <zackarynichol@gmail.com>
2020-02-17 22:15:30 -0600

    Added gitignore

--- src/Proj4.java
+++ src/Proj4.java
@@ -1,5 +1,45 @@
 public class Proj4 {
 
+    // Royal Flush
+    // int[] value = {10, 12, 14, 13, 11};
+    // int[] suit = {1,1,1,1,1};
+
+    // Straight Flush
+    // int[] value = {9, 7, 8, 6, 5};
+    // int[] suit = {1,1,1,1,1};
+
+    // 4 of kind
+    // int[] value = {9, 7, 9, 9, 9};
+    // int[] suit = {1,1,3,4,2};
+
+    // Full House
+    // int[] value = {9, 7, 9, 7, 9};
+    // int[] suit = {1,2,3,4,2};
+
+    // Flush
+    // int[] value = {9, 10, 8, 6, 5};
+    // int[] suit = {1,1,1,1,1};
+
+    // Straight
+    // int[] value = {9, 7, 8, 6, 5};
+    // int[] suit = {1,2,4,3,1};
+
+    // 3 of kind
+    // int[] value = {9, 7, 9, 2, 9};
+    // int[] suit = {1,2,3,4,2};
+
+    // Two Pair
+    // int[] value = {9, 7, 9, 2, 7};
+    // int[] suit = {1,2,3,4,1};
+
+    // One Pair
+    // int[] value = {9, 7, 8, 2, 7};
+    // int[] suit = {1,2,3,4,1};
+
+    // High Card (Ace)
+    // int[] value = {9, 7, 8, 14, 11};
+    // int[] suit = {1,2,3,4,1};
+
     public static void main(String[] args) {
 
     }
